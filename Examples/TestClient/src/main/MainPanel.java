b0e422dd4ecd879471c3adcfe4280ffd6999eb2a
jaimesDSG <jaimes.dsg@gmail.com>
2012-06-08 15:47:25 +0200

    modif hMessageOption to hMessageOptions and equal() of hLocation

--- Examples/TestClient/src/main/MainPanel.java
+++ Examples/TestClient/src/main/MainPanel.java
@@ -55,13 +55,13 @@ public class MainPanel extends JPanel {
 	private HOptions option = new HOptions();
 	private CallbackExample callback = new CallbackExample(this);
 
-	private JTextField usernameField = new JTextField("u1@hub.novediagroup.com");
-	private JTextField passwordField = new JTextField("u1");
-	private JTextField endPointField = new JTextField("http://hub.novediagroup.com:8080");
+	private JTextField usernameField = new JTextField("");
+	private JTextField passwordField = new JTextField("");
+	private JTextField endPointField = new JTextField("");
 	private JTextField serverPortField = new JTextField("");
 	private JTextField serverHostField = new JTextField("");
-	private JTextField chidField = new JTextField("channel");
-	private JTextField messageField = new JTextField("test");
+	private JTextField chidField = new JTextField("");
+	private JTextField messageField = new JTextField("");
 	private JTextField nbLastMessagesField = new JTextField("");
 	
 	
