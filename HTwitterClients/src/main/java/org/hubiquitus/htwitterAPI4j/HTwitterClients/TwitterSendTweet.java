ed57b859c836975c53e6a841171d7d6a70cb7823
Adel MANI <a.mani@novedia-solutions.com>
2012-11-15 10:02:30 -0500

    removed  unnecessary attributes

--- HTwitterClients/src/main/java/org/hubiquitus/htwitterAPI4j/HTwitterClients/TwitterSendTweet.java
+++ HTwitterClients/src/main/java/org/hubiquitus/htwitterAPI4j/HTwitterClients/TwitterSendTweet.java
@@ -8,37 +8,32 @@ public class TwitterSendTweet {
 
 	final static Logger log = LoggerFactory.getLogger(TwitterSendTweet.class);
 
-	
-
 	private static String consumerKey             = "yourConsumerKey";
 	private static String consumerSecret          = "yourConsumerSecret";
 	private static String twitterAccessToken      = "yourAccessToken";
 	private static String twitterAccessTokenSecret= "yourAccessTokenSecret";	
+
 	
-	private static String tags="tag1,tag2,..";
-	
+
 	private static String proxyHost="0.0.0.0"; //Optional
 	private static int    proxyPort= 0000;     //Optional
-	
-	private static String delimited="all";                         //Optional
-	private static String stallWarnings="true";                    //Optional
-	private static String with="followings";                       //Optional 
-	private static String replies="all";                           //Optional
-	private static String locations="-122.75,36.8,-121.75,37.8";   //Optional
-	private static String count="130000";                          //Optional
 
-	private static String status="Hello Hubiquitus 2 !!!";
+	private static String status=" Best wishes from Hubiquitus Team  !!!";
 	private static String screenName ="@testMANI1";
+
 
 	public static void main(String[] args) {
 
 		status = screenName+" "+status;
 
-		HStatusUpdate updateStatus = new HStatusUpdate(proxyHost, proxyPort, consumerKey, consumerSecret, twitterAccessToken, twitterAccessTokenSecret, status);
-		int code = updateStatus.postTweet();
-
-		log.info(" RECIVED CODE : "+code);
-
+		HStatusUpdate updateStatus = new HStatusUpdate(proxyHost, proxyPort, consumerKey, consumerSecret, twitterAccessToken, twitterAccessTokenSecret);
+		int code = updateStatus.postTweet(status);
+		if(code == 200){
+			log.info(" Your tweet is posted with succes. Code : "+code);	
+		}else{
+			log.info(" We can't delivred your tweet, May be you are alerdy tweet this text. Code error  : "+code);
+		}
+		
 
 	}
 
